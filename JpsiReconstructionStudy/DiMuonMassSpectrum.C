734fa25122b8efb5b7fab319ff11061cc065fa29
Paul Veen <paul.veen@yahoo.com>
2024-11-15 15:59:36 +0100

    Update DiMuonMassSpectrum.C

--- JpsiReconstructionStudy/DiMuonMassSpectrum.C
+++ JpsiReconstructionStudy/DiMuonMassSpectrum.C
@@ -22,6 +22,7 @@
 #include "RooCrystalBall.h"
 #include "RooChebychev.h"
 #include "RooFitResult.h"
+#include "RooMsgService.h"
 
 using namespace RooFit;
 
@@ -56,12 +57,22 @@ void fitJpsiGauss(TH1D* hist, Double_t pTMin, Double_t pTMax) {
 } // fitJpsiGauss
 
 void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
+    /*
+    TCanvas* test = new TCanvas(Form("test_Pt_%.0f_%.0f", pTMin, pTMax), 
+                                Form("test_Pt_%.0f_%.0f", pTMin, pTMax));
+    test->cd();
+    hist->Draw();
+    */
+
     // Initialise
     Double_t mMin = 2;
     Double_t mMax = 5;
-    RooRealVar m("m", "invariant mass", mMin, mMax);
+    RooRealVar m(Form("m_Pt_%.0f_%.0f", pTMin, pTMax), "invariant mass", mMin, mMax);
     m.setRange("fitRange", 2, 5);
-    RooDataHist* data = new RooDataHist("data", "Di-muon spectrum", m, Import(*hist));
+    RooDataHist* data = new RooDataHist(Form("data_Pt_%.0f_%.0f", pTMin, pTMax), "Di-muon spectrum", m, Import(*hist));
+
+    RooAddPdf* model;
+    RooPlot* frame;
 
     // Crystal Ball
     RooRealVar m0("m0", "Mean", 3.097, 3.05, 3.13);
@@ -75,44 +86,64 @@ void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
     RooRealVar nR("nR", "Exponent Right", 15.323, 5.0, 25.0);  
     nR.setConstant();
 
-    RooCrystalBall* doubleSidedCB = new RooCrystalBall("doubleSidedCB", "Double Sided Crystal Ball", m, m0, sigma, alphaL, nL, alphaR, nR);
+    RooCrystalBall* doubleSidedCB = new RooCrystalBall(Form("doubleSidedCB_Pt_%.0f_%.0f", pTMin, pTMax), 
+                                                            "Double Sided Crystal Ball", m, m0, sigma, alphaL, nL, alphaR, nR);
     // doubleSidedCB->fitTo(*data);
 
     // Exponential for background
     // RooRealVar a0("a0","a_{0}",-1,-5,0.000001);
 	// RooExponential* BKG = new RooExponential("BKG","Power background", m, a0);
 
-    // Initializing Chebyshev polynomials for background
-    RooRealVar a0("a0","a_{0}",-0.01,-1.0,1.0);
-	RooRealVar a1("a1","a_{1}",-0.01,-1.0,1.0);
-	RooRealVar a2("a2","a_{2}",-0.01,-1.0,1.0); // essentially best result here, by iterative fitting
-	// RooRealVar a3("a3","a_{3}",0.1,-2.0,2.0);
-	// RooRealVar a4("a4","a_{4}",0.0,-2.0,2.0);
-	//RooRealVar a5("a5","a_{5}",0.0,-2.0,2.0);
-	//RooRealVar a6("a6","a_{6}",0.0,-2.0,2.0);
-	//RooRealVar a7("a7","a_{7}",0.0,-2.0,2.0);
-	RooChebychev* BKG = new RooChebychev("BKG","Chebyshev background", m, {a0,a1,a2});
+    RooRealVar a0("a0", "a_{0}", -0.01, -2.0, 2.0);
+    RooRealVar a1("a1", "a_{1}", 0.0, -1.0, 1.0);
+    RooRealVar a2("a2", "a_{2}", 0.0, -0.5, 0.5);
+    RooRealVar a3("a3", "a_{3}", 0.0, -0.5, 0.5);
+    RooRealVar a4("a4", "a_{4}", 0.0, -0.3, 0.3);
+    RooRealVar a5("a5", "a_{5}", 0.0, -0.3, 0.3);
+    RooRealVar a6("a6", "a_{6}", 0.0, -0.1, 0.1);
+    RooRealVar a7("a7", "a_{7}", 0.0, -0.05, 0.05);
+
+    // Initially set all parameters to constant (kTRUE)
+    std::vector<RooRealVar*> parameters = {&a0, &a1, &a2, &a3, &a4, &a5, &a6, &a7};
+    for (auto& param : parameters) {
+        param->setConstant(kTRUE);
+    }
+
+    RooChebychev* BKG = new RooChebychev(Form("BKG_Pt_%.0f_%.0f", pTMin, pTMax),"Chebyshev background", m, {a0,a1,a2,a3,a4,a5,a6,a7});
 
     // Add them
-    RooRealVar sigYield("sigYield", "N_{sig}", 50, 0., 100000000);
-   	RooRealVar bkgYield("bkgYield", "N_{bkg}", 10000, 0., 1000000000);
-    RooAddPdf model("model", "combined mass model", {*doubleSidedCB, *BKG}, {sigYield, bkgYield});
-    model.fitTo(*data, Range("fitRange"));
+    RooRealVar sigYield("sigYield", "N_{sig}", 50000, 0., 100000000);
+    RooRealVar bkgYield("bkgYield", "N_{bkg}", 1000000, 0., 1000000000);
+    model = new RooAddPdf(Form("model_Pt_%.0f_%.0f", pTMin, pTMax), "combined mass model", 
+                                {*doubleSidedCB, *BKG}, {sigYield, bkgYield});
 
-    // Plot
-    RooPlot* frame = m.frame();
-    data->plotOn(frame, MarkerSize(0.4));
-    model.plotOn(frame, LineColor(kBlue), Name("full_Model"));
-    double chi2M = frame->chiSquare(); // reduced χ2 (?)
-    // doubleSidedCB->plotOn(frame);
+    // Start iterative fitting
+    // Release orders one-by-one as long as reduced χ2 is above desired value
+    Double_t chi2M = 100000; // arbitrary starting value
+
+    for (Int_t i = 0; i < parameters.size(); i++) {
+        if (chi2M < 0 || chi2M > 1.4) {
+            parameters[i]->setConstant(kFALSE);
+            model->fitTo(*data, Range("fitRange"));
+            frame = m.frame();
+            data->plotOn(frame, MarkerSize(0.4));
+            model->plotOn(frame, LineColor(kBlue), Name("full_Model"));
+            chi2M = frame->chiSquare();
+            std::cout << "chi2M = " << chi2M << std::endl;
+        }
+        else { break; }
+        if (i == 7 && (chi2M < 0 || chi2M > 1.4)) { std::cout << "WARNING. Fitting failed. No convergence" << std::endl; }
+    }
+
+    // Plot everything
     TCanvas* canvas = new TCanvas(Form("canvas_Pt_%.0f_%.0f", pTMin, pTMax), 
                                   Form("Double Sided Crystal Ball Fit %.0f < p_{T} < %.0f", pTMin, pTMax),
                                   800, 600);
     canvas->cd();
 
-    model.plotOn(frame, Components(*doubleSidedCB), LineStyle(kDashed), LineColor(kRed), Name("signal_Model"));
-   	model.plotOn(frame, Components(*BKG), LineStyle(kDashed), LineColor(kBlue), Name("bkg_Model"));
-   	model.paramOn(frame,ShowConstants(true),Format("TE",AutoPrecision(3)));
+    model->plotOn(frame, Components(*doubleSidedCB), LineStyle(kDashed), LineColor(kRed), Name("signal_Model"));
+   	model->plotOn(frame, Components(*BKG), LineStyle(kDashed), LineColor(kBlue), Name("bkg_Model"));
+   	model->paramOn(frame,ShowConstants(true),Format("TE",AutoPrecision(3)));
 
     frame->Draw();
 
@@ -149,6 +180,8 @@ void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
 
 int DiMuonMassSpectrum() 
  {
+    // Set RooFit to minimal messages
+    RooMsgService::instance().setGlobalKillBelow(RooFit::ERROR);
 
 
     // **********************************************
@@ -157,31 +190,35 @@ int DiMuonMassSpectrum()
 
 
     THashList *listDiMuon;
-    TList *subListDiMuonPM;
+    TList *subListDiMuon;
     TFile* fDiMuon = new TFile("AnalysisResults.root", "READ");
     listDiMuon = (THashList*)fDiMuon->Get("analysis-same-event-pairing/output");
-    subListDiMuonPM = (TList*)listDiMuon->FindObject("PairsMuonSEPM_muonLowPt510SigmaPDCA");
+    subListDiMuon = (TList*)listDiMuon->FindObject("PairsMuonSEPM_muonLowPt510SigmaPDCA");
 
-    TH2F *hMass_Pt_PM;
-    hMass_Pt_PM = (TH2F*)subListDiMuonPM->FindObject("Mass_Pt");
+    TH2F *hMass_Pt;
+    hMass_Pt = (TH2F*)subListDiMuon->FindObject("Mass_Pt");
 
 
     // **********************************************
     // S  E  T    P  _  T    C  U  T  S
     // **********************************************
 
-    TH2F *hMass_Pt_PM_Pt_0_2,    *hMass_Pt_PM_Pt_2_5,    *hMass_Pt_PM_Pt_5_30;
-    TH1D *hDiMuonMass_PM_Pt_0_2, *hDiMuonMass_PM_Pt_2_5, *hDiMuonMass_PM_Pt_5_30;
 
-    hMass_Pt_PM->GetYaxis()->SetRangeUser(0, 2);
-    hDiMuonMass_PM_Pt_0_2 = hMass_Pt_PM->ProjectionX("hDiMuonMass_PM");
+    TH2F *hMass_Pt_PtCut_0_2,    *hMass_Pt_PtCut_3_4,    *hMass_Pt_PtCut_5_30;
+    TH1D *hDiMuonMass_PtCut_0_2, *hDiMuonMass_PtCut_3_4, *hDiMuonMass_PtCut_5_30;
 
-    hMass_Pt_PM->GetYaxis()->SetRangeUser(2, 5);
-    hDiMuonMass_PM_Pt_2_5 = hMass_Pt_PM->ProjectionX("hDiMuonMass_PM");
+    hMass_Pt_PtCut_0_2 = (TH2F*)hMass_Pt->Clone();
+    hMass_Pt_PtCut_0_2->GetYaxis()->SetRangeUser(0, 2);
+    hDiMuonMass_PtCut_0_2 = hMass_Pt_PtCut_0_2->ProjectionX("hDiMuonMass_PtCut_0_2");
 
-    hMass_Pt_PM->GetYaxis()->SetRangeUser(5, 30);
-    hDiMuonMass_PM_Pt_5_30 = hMass_Pt_PM->ProjectionX("hDiMuonMass_PM");
-    
+    hMass_Pt_PtCut_3_4 = (TH2F*)hMass_Pt->Clone();
+    hMass_Pt_PtCut_3_4->GetYaxis()->SetRangeUser(3, 4);
+    hDiMuonMass_PtCut_3_4 = hMass_Pt_PtCut_3_4->ProjectionX("hDiMuonMass_PtCut_3_4");
+
+    hMass_Pt_PtCut_5_30 = (TH2F*)hMass_Pt->Clone();
+    hMass_Pt_PtCut_5_30->GetYaxis()->SetRangeUser(5, 30);
+    hDiMuonMass_PtCut_5_30 = hMass_Pt_PtCut_5_30->ProjectionX("hDiMuonMass_PtCut_5_30");
+
 
 
     // **********************************************
@@ -190,13 +227,13 @@ int DiMuonMassSpectrum()
 
 
     // fitJpsiGauss(hDiMuonMass_PM_Pt_0_2, 0, 2);
-    fitJpsiCB(hDiMuonMass_PM_Pt_0_2, 0, 2);
+    fitJpsiCB(hDiMuonMass_PtCut_0_2,  0, 2);
 
     // fitJpsiGauss(hDiMuonMass_PM_Pt_2_5, 2, 5);
-    fitJpsiCB(hDiMuonMass_PM_Pt_2_5, 2, 5);
+    fitJpsiCB(hDiMuonMass_PtCut_3_4,  3, 4);
 
     // fitJpsiGauss(hDiMuonMass_PM_Pt_5_30, 5, 30);
-    fitJpsiCB(hDiMuonMass_PM_Pt_5_30, 5, 30);
+    fitJpsiCB(hDiMuonMass_PtCut_5_30, 5, 30);
 
     return 0;
  }
