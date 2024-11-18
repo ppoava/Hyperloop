ddfce31b82e4cfed8e7fe8f11943c2e941d1eaf7
Paul Veen <paul.veen@yahoo.com>
2024-11-18 12:00:50 +0100

    Update DiMuonMassSpectrum.C

--- JpsiReconstructionStudy/DiMuonMassSpectrum.C
+++ JpsiReconstructionStudy/DiMuonMassSpectrum.C
@@ -19,6 +19,7 @@
 #include "RooExponential.h"
 #include "RooChi2Var.h"
 #include "RooGaussian.h"
+#include "RooFormulaVar.h"
 #include "RooCrystalBall.h"
 #include "RooChebychev.h"
 #include "RooFitResult.h"
@@ -56,15 +57,14 @@ void fitJpsiGauss(TH1D* hist, Double_t pTMin, Double_t pTMax) {
     result->Print(); 
 } // fitJpsiGauss
 
-void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
-    /*
-    TCanvas* test = new TCanvas(Form("test_Pt_%.0f_%.0f", pTMin, pTMax), 
-                                Form("test_Pt_%.0f_%.0f", pTMin, pTMax));
-    test->cd();
-    hist->Draw();
-    */
+void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax, std::string BKG_model) {
 
-    // Initialise
+
+    // **********************************************
+    // I  N  I  T  I  A  L  I  S  A  T  I  O  N
+    // **********************************************
+
+
     Double_t mMin = 2;
     Double_t mMax = 5;
     RooRealVar m(Form("m_Pt_%.0f_%.0f", pTMin, pTMax), "invariant mass", mMin, mMax);
@@ -86,14 +86,23 @@ void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
     RooRealVar nR("nR", "Exponent Right", 15.323, 5.0, 25.0);  
     nR.setConstant();
 
+    // Signal model
     RooCrystalBall* doubleSidedCB = new RooCrystalBall(Form("doubleSidedCB_Pt_%.0f_%.0f", pTMin, pTMax), 
                                                             "Double Sided Crystal Ball", m, m0, sigma, alphaL, nL, alphaR, nR);
-    // doubleSidedCB->fitTo(*data);
+
+
+    // **********************************************
+    // G  E  T    D  A  T  A
+    // **********************************************
+
+
+    RooAbsPdf* BKG;
 
     // Exponential for background
     // RooRealVar a0("a0","a_{0}",-1,-5,0.000001);
 	// RooExponential* BKG = new RooExponential("BKG","Power background", m, a0);
 
+    // Initiallise Chebychev
     RooRealVar a0("a0", "a_{0}", -0.01, -2.0, 2.0);
     RooRealVar a1("a1", "a_{1}", 0.0, -1.0, 1.0);
     RooRealVar a2("a2", "a_{2}", 0.0, -0.5, 0.5);
@@ -104,12 +113,29 @@ void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
     RooRealVar a7("a7", "a_{7}", 0.0, -0.05, 0.05);
 
     // Initially set all parameters to constant (kTRUE)
+    // Iterative fitting releases parameters one-by-one
     std::vector<RooRealVar*> parameters = {&a0, &a1, &a2, &a3, &a4, &a5, &a6, &a7};
     for (auto& param : parameters) {
         param->setConstant(kTRUE);
     }
 
-    RooChebychev* BKG = new RooChebychev(Form("BKG_Pt_%.0f_%.0f", pTMin, pTMax),"Chebyshev background", m, {a0,a1,a2,a3,a4,a5,a6,a7});
+    // Initialise a variable width Gaussian for background
+    // Define width = base + slope * m
+        std::cout<<"hoi"<<std::endl;
+        RooRealVar BKG_mean("BKG_mean", "Mean of background Gaussian", 2.2);
+        RooRealVar BKG_base("BKG_base", "Base of variable width Gaussian", 1.0, 0.1, 10.0);
+        RooRealVar BKG_slope("BKG_slope", "Slope of variable width Gaussian", 0.1, -1.0, 1.0);
+        RooFormulaVar BKG_sigma("BKG_sigma", "Variable width of Background Gaussian", "@0 + @1*@2", 
+                                RooArgList(BKG_base, BKG_slope, m));
+    
+    // Select background model (all are initialised, one is picked)
+    if (BKG_model == "Chebychev") {
+        BKG = new RooChebychev(Form("BKG_Pt_%.0f_%.0f", pTMin, pTMax),"Chebyshev background", m, {a0,a1,a2,a3,a4,a5,a6,a7});
+    }
+
+    if (BKG_model == "Gaussian") {
+        BKG = new RooGaussian("gauss", "Gaussian with variable width", m, BKG_mean, BKG_sigma);
+    }
 
     // Add them
     RooRealVar sigYield("sigYield", "N_{sig}", 50000, 0., 100000000);
@@ -117,25 +143,40 @@ void fitJpsiCB(TH1D* hist, Double_t pTMin, Double_t pTMax) {
     model = new RooAddPdf(Form("model_Pt_%.0f_%.0f", pTMin, pTMax), "combined mass model", 
                                 {*doubleSidedCB, *BKG}, {sigYield, bkgYield});
 
-    // Start iterative fitting
+    // Start iterative fitting (only for Chebychev)
     // Release orders one-by-one as long as reduced χ2 is above desired value
     Double_t chi2M = 100000; // arbitrary starting value
-
-    for (Int_t i = 0; i < parameters.size(); i++) {
-        if (chi2M < 0 || chi2M > 1.4) {
-            parameters[i]->setConstant(kFALSE);
-            model->fitTo(*data, Range("fitRange"));
-            frame = m.frame();
-            data->plotOn(frame, MarkerSize(0.4));
-            model->plotOn(frame, LineColor(kBlue), Name("full_Model"));
-            chi2M = frame->chiSquare();
-            std::cout << "chi2M = " << chi2M << std::endl;
+    if (BKG_model == "Chebychev") {
+        for (Int_t i = 0; i < parameters.size(); i++) {
+            if (chi2M < 0 || chi2M > 1.4) {
+                parameters[i]->setConstant(kFALSE);
+                model->fitTo(*data, Range("fitRange"));
+                frame = m.frame();
+                data->plotOn(frame, MarkerSize(0.4));
+                model->plotOn(frame, LineColor(kBlue), Name("full_Model"));
+                chi2M = frame->chiSquare();
+                std::cout << "chi2M = " << chi2M << std::endl;
+            }
+            else { break; }
+            if (i == 7 && (chi2M < 0 || chi2M > 1.4)) { std::cout << "WARNING. Fitting failed. No convergence" << std::endl; }
         }
-        else { break; }
-        if (i == 7 && (chi2M < 0 || chi2M > 1.4)) { std::cout << "WARNING. Fitting failed. No convergence" << std::endl; }
     }
 
-    // Plot everything
+    if (BKG_model != "Chebychev") {
+        model->fitTo(*data, Range("fitRange"));
+        frame = m.frame();
+        data->plotOn(frame, MarkerSize(0.4));
+        model->plotOn(frame, LineColor(kBlue), Name("full_Model"));
+        chi2M = frame->chiSquare();
+        std::cout << "chi2M = " << chi2M << std::endl;
+    }
+
+
+    // **********************************************
+    // P  L  O  T  T  I  N  G
+    // **********************************************
+
+
     TCanvas* canvas = new TCanvas(Form("canvas_Pt_%.0f_%.0f", pTMin, pTMax), 
                                   Form("Double Sided Crystal Ball Fit %.0f < p_{T} < %.0f", pTMin, pTMax),
                                   800, 600);
@@ -227,13 +268,13 @@ int DiMuonMassSpectrum()
 
 
     // fitJpsiGauss(hDiMuonMass_PM_Pt_0_2, 0, 2);
-    fitJpsiCB(hDiMuonMass_PtCut_0_2,  0, 2);
+    fitJpsiCB(hDiMuonMass_PtCut_0_2,  0, 2, "Gaussian");
 
     // fitJpsiGauss(hDiMuonMass_PM_Pt_2_5, 2, 5);
-    fitJpsiCB(hDiMuonMass_PtCut_3_4,  3, 4);
+    fitJpsiCB(hDiMuonMass_PtCut_3_4,  3, 4, "Chebychev");
 
     // fitJpsiGauss(hDiMuonMass_PM_Pt_5_30, 5, 30);
-    fitJpsiCB(hDiMuonMass_PtCut_5_30, 5, 30);
+    fitJpsiCB(hDiMuonMass_PtCut_5_30, 5, 30, "Chebychev");
 
     return 0;
  }
