a81c866c3c4a50af82eaf14801199aa19582824c
Paul Veen <paul.veen@yahoo.com>
2025-02-18 11:49:17 +0100

    Update compareKinematics.C

--- Matching/MC/promptNonPromptJpsiPsi2S/compareKinematics.C
+++ Matching/MC/promptNonPromptJpsiPsi2S/compareKinematics.C
@@ -15,34 +15,18 @@
 #include "TLegend.h"
 #include "TPad.h"
 #include "TLine.h"
-
-#include "RooRealVar.h"
-#include "RooDataHist.h"
-#include "RooHist.h"
-#include "RooPlot.h"
-#include "RooAddPdf.h"
-#include "RooExponential.h"
-#include "RooChi2Var.h"
-#include "RooGaussian.h"
-#include "RooFormulaVar.h"
-#include "RooCrystalBall.h"
-#include "RooChebychev.h"
-#include "RooFitResult.h"
-#include "RooMsgService.h"
-#include "RooWorkspace.h"
+#include "THnSparse.h"
 
 
-TH1D* getTree(const char* fileName, Double_t ptMin, Double_t ptMax) {
+
+TH1D* getHistFromTree(const char* fileName, Double_t ptMin, Double_t ptMax) {
 
 
 THashList *listDiMuon;
 TList *subListDiMuon;
-TH2F *hMass_Pt;
-
 TFile *fDiMuon = new TFile(fileName,"READ");
 listDiMuon = (THashList*)fDiMuon->Get("analysis-same-event-pairing/output");
 subListDiMuon = (TList*)listDiMuon->FindObject("PairsMuonSEPM_muonQualityCuts");
-hMass_Pt = (TH2F*)subListDiMuon->FindObject("Mass_Pt");
 
 
 // **********************************************
@@ -50,26 +34,71 @@ hMass_Pt = (TH2F*)subListDiMuon->FindObject("Mass_Pt");
 // **********************************************
 
 
+// Approach with "dimuon" histogram option in same-event-pairing
+/*
+TH2F *hMass_Pt;
+hMass_Pt = (TH2F*)subListDiMuon->FindObject("Mass_Pt");
 TH1D *hDiMuonMass_PtCut;
-
 hMass_Pt = (TH2F*)hMass_Pt->Clone();
 hMass_Pt->GetYaxis()->SetRangeUser(ptMin,ptMax);
 hDiMuonMass_PtCut = hMass_Pt->ProjectionX("hDiMuonMass_PtCut");
 hDiMuonMass_PtCut->GetXaxis()->SetRangeUser(0,8); // plot only relevant mass range
+*/
 
 
-return hDiMuonMass_PtCut;
+// Approach with "dimuon-rap" histogram option in same-event-pairing
+THnSparse *hMass_Pt_Cent_Rap;
+hMass_Pt_Cent_Rap = (THnSparse*)subListDiMuon->FindObject("Mass_Pt_Cent_Rap");
+THnSparse* hSparse = (THnSparse*)hMass_Pt_Cent_Rap->Clone();
+
+// Define the axis indices
+const int kMassAxis = 0;  // Mass axis index
+const int kPtAxis = 1;    // pT axis index
+const int kCentAxis = 2;  // Centrality axis index
+const int kRapAxis = 3;   // Rapidity axis index
+
+// Set axis ranges
+// double massMin = 0., massMax = 8.;  // Example mass range
+double ptCutMin = 0., ptCutMax = 15.;        // Example pT range
+// double rapMin = -0.8, rapMax = 0.8; // Example rapidity range
+// double centMin = 0, centMax = 10;   // Example centrality range
+// Apply axis range cuts
+// hSparse->GetAxis(kMassAxis)->SetRangeUser(massMin, massMax);
+hSparse->GetAxis(kPtAxis)->SetRangeUser(ptMin, ptMax);
+// hSparse->GetAxis(kRapAxis)->SetRangeUser(rapMin, rapMax);
+// hSparse->GetAxis(kCentAxis)->SetRangeUser(centMin, centMax);
+
+// Project onto the pT axis
+TH1D *hist;
+hist = (TH1D*)hSparse->Projection(kPtAxis);
+// hist = (TH1D*)hSparse->Projection(kMassAxis);
+// hist = (TH1D*)hSparse->Projection(kRapAxis);
 
 
-} // getTree()
+return hist;
+
+
+} // getHistFromTree()
 
 
 TCanvas* plotHist(TH1D *hNonPrompt, TH1D *hPrompt) {
 
 
+    hNonPrompt->Scale(1/hNonPrompt->GetEntries());
+    hNonPrompt->SetLineColor(kRed);
+    hPrompt->Scale(1/hPrompt->GetEntries());
+
+
     TCanvas *canvas = new TCanvas("canvas", "canvas", 800, 600);
     canvas->cd();
-    hNonPrompt->Draw("HIST E");
+    hPrompt->Draw("HIST E");
+    hNonPrompt->Draw("SAME HIST E");
+    
+
+    TLegend *legend = new TLegend(0.80,0.60,0.90,0.75);
+    legend->AddEntry(hNonPrompt, "non-prompt", "l");
+    legend->AddEntry(hPrompt, "prompt", "l");
+    legend->Draw();
 
 
     return canvas;
@@ -84,8 +113,9 @@ int compareKinematics() {
     std::cout<<"running compareKinematics macro"<<std::endl;
 
 
-    TH1D *hist = getTree("/Users/pv280546/alice/nonPromptJpsiTest_AnalysisResults-3.root", 0, 30);
-    TCanvas *canvas = plotHist(hist, hist);
+    TH1D *hNonPrompt = getHistFromTree("/Users/pv280546/alice/nonPromptJpsiTest_AnalysisResults-3.root", 0, 30);
+    TH1D *hPrompt = getHistFromTree("/Users/pv280546/alice/promptJpsiTest_AnalysisResults-3.root", 0, 30);
+    TCanvas *canvas = plotHist(hNonPrompt, hPrompt);
     canvas->Draw();
 
 
